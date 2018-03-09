04ee9a5f7d06ea6dc77d9c9535dadc98b852c002
zhangqin <1245366907@qq.com>
2018-03-09 14:24:15 +0800

    fix bug

--- quicklib/src/main/java/com/sdwfqin/quicklib/utils/ImageLoader.java
+++ quicklib/src/main/java/com/sdwfqin/quicklib/utils/ImageLoader.java
@@ -2,21 +2,11 @@ package com.sdwfqin.quicklib.utils;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.Request;
-import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.target.SizeReadyCallback;
-import com.bumptech.glide.request.target.Target;
-import com.bumptech.glide.request.transition.Transition;
 import com.sdwfqin.quicklib.GlideApp;
 import com.sdwfqin.quicklib.R;
 import com.sdwfqin.quicklib.base.Constants;
@@ -68,7 +58,7 @@ public class ImageLoader {
         GlideApp
                 .with(context)
                 .load(model)
-                .placeholder(R.mipmap.ic_launcher)
+                .placeholder(R.mipmap.quicklib_launcher)
                 .into(imageView);
     }
 
