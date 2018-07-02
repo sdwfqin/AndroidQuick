7c10e529e65b0aae0e34d1af8a84b49f03a228b0
zhangqin <1245366907@qq.com>
2018-07-02 15:25:50 +0800

    widget

--- widget/src/main/java/com/sdwfqin/widget/WrapContentHeightViewPager.java
+++ widget/src/main/java/com/sdwfqin/widget/WrapContentHeightViewPager.java
@@ -3,6 +3,7 @@ package com.sdwfqin.widget;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -12,6 +13,11 @@ import android.view.View;
  * @date 2018/3/2
  */
 public class WrapContentHeightViewPager extends ViewPager {
+
+    /**
+     * true 禁止滑动
+     */
+    private boolean noScroll = false;
 
     public WrapContentHeightViewPager(Context context) {
         super(context);
@@ -36,5 +42,46 @@ public class WrapContentHeightViewPager extends ViewPager {
         }
         heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * 设置是否允许滑动
+     * <p>
+     * true 禁止滑动
+     *
+     * @param noScroll
+     */
+    public void setNoScroll(boolean noScroll) {
+        this.noScroll = noScroll;
+    }
+
+    public boolean getNoScroll() {
+        return noScroll;
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        return !noScroll && super.onTouchEvent(arg0);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        return !noScroll && super.onInterceptTouchEvent(arg0);
+    }
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        super.setCurrentItem(item, smoothScroll);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        //false 去除滚动效果
+        super.setCurrentItem(item, false);
     }
 }
