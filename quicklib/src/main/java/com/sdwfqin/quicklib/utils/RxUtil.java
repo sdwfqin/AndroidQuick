5daff04eab9543ae11ececca51502d275894ba2f
zhangqin <1245366907@qq.com>
2018-07-11 16:44:13 +0800

    添加侧滑删除Activity

--- quicklib/src/main/java/com/sdwfqin/quicklib/utils/RxUtil.java
+++ quicklib/src/main/java/com/sdwfqin/quicklib/utils/RxUtil.java
@@ -1,13 +1,14 @@
 package com.sdwfqin.quicklib.utils;
 
 import io.reactivex.FlowableTransformer;
+import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 /**
  * 描述：RxJava工具类
  *
- * @author  张钦
+ * @author 张钦
  */
 public class RxUtil {
 
@@ -18,6 +19,19 @@ public class RxUtil {
      * @return
      */
     public static <T> FlowableTransformer<T, T> rxSchedulerHelper() {
+
+        //compose简化线程
+        return observable -> observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    /**
+     * 统一线程处理
+     *
+     * @param <T>
+     * @return
+     */
+    public static <T> ObservableTransformer<T, T> rxObservableSchedulerHelper() {
 
         //compose简化线程
         return observable -> observable.subscribeOn(Schedulers.io())
