32a2f99df97ec551fff53992d8325b45d812ce26
zhangqin <1245366907@qq.com>
2018-08-30 17:20:19 +0800

    图片添加水印工具类

---
+++ quicklib/src/main/java/com/sdwfqin/quicklib/imagewatermark/ImageWatermarkUtils.java
@@ -0,0 +1,158 @@
+package com.sdwfqin.quicklib.imagewatermark;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * 描述：图片水印工具类
+ *
+ * @author 张钦
+ * @date 2018/8/30
+ */
+public class ImageWatermarkUtils {
+
+    /**
+     * 给图片添加文字到左上角
+     *
+     * @param context
+     * @param bitmap
+     * @param text
+     * @return
+     */
+    public static Bitmap drawTextToLeftTop(Context context, Bitmap bitmap, String text, int size,
+                                           int color, int paddingLeft, int paddingTop) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        paint.setTextSize(size);
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        return drawTextToBitmap(context, bitmap, text, paint, bounds, paddingLeft,
+                paddingTop + bounds.height());
+    }
+
+    /**
+     * 绘制文字到右下角
+     *
+     * @param context
+     * @param bitmap
+     * @param text
+     * @param size
+     * @param color
+     * @param paddingRight
+     * @param paddingBottom
+     * @return
+     */
+    public static Bitmap drawTextToRightBottom(Context context, Bitmap bitmap, String text, int size,
+                                               int color, int paddingRight, int paddingBottom) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        paint.setTextSize(size);
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        return drawTextToBitmap(context, bitmap, text, paint, bounds,
+                bitmap.getWidth() - bounds.width() - paddingRight, bitmap.getHeight() - paddingBottom);
+    }
+
+    /**
+     * 绘制文字到右上方
+     *
+     * @param context
+     * @param bitmap
+     * @param text
+     * @param size
+     * @param color
+     * @param paddingRight
+     * @param paddingTop
+     * @return
+     */
+    public static Bitmap drawTextToRightTop(Context context, Bitmap bitmap, String text, int size,
+                                            int color, int paddingRight, int paddingTop) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        paint.setTextSize(size);
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        return drawTextToBitmap(context, bitmap, text, paint, bounds,
+                bitmap.getWidth() - bounds.width() - paddingRight,
+                paddingTop + bounds.height());
+    }
+
+    /**
+     * 绘制文字到左下方
+     *
+     * @param context
+     * @param bitmap
+     * @param text
+     * @param size
+     * @param color
+     * @param paddingLeft
+     * @param paddingBottom
+     * @return
+     */
+    public static Bitmap drawTextToLeftBottom(Context context, Bitmap bitmap, String text, int size,
+                                              int color, int paddingLeft, int paddingBottom) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        paint.setTextSize(size);
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        return drawTextToBitmap(context, bitmap, text, paint, bounds, paddingLeft,
+                bitmap.getHeight() - paddingBottom);
+    }
+
+    /**
+     * 绘制文字到中间
+     *
+     * @param context
+     * @param bitmap
+     * @param text
+     * @param size
+     * @param color
+     * @return
+     */
+    public static Bitmap drawTextToCenter(Context context, Bitmap bitmap, String text, int size,
+                                          int color) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(color);
+        paint.setTextSize(size);
+        Rect bounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), bounds);
+        return drawTextToBitmap(context, bitmap, text, paint, bounds,
+                (bitmap.getWidth() - bounds.width()) / 2, (bitmap.getHeight() + bounds.height()) / 2);
+    }
+
+    /**
+     * 图片上绘制文字
+     *
+     * @param context
+     * @param bitmap
+     * @param text
+     * @param paint
+     * @param bounds
+     * @param paddingLeft
+     * @param paddingTop
+     * @return
+     */
+    private static Bitmap drawTextToBitmap(Context context, Bitmap bitmap, String text, Paint paint,
+                                           Rect bounds, int paddingLeft, int paddingTop) {
+        Bitmap.Config bitmapConfig = bitmap.getConfig();
+
+        paint.setDither(true); // 获取跟清晰的图像采样
+        paint.setFilterBitmap(true);// 过滤一些
+        if (bitmapConfig == null) {
+            bitmapConfig = Bitmap.Config.ARGB_8888;
+        }
+        bitmap = bitmap.copy(bitmapConfig, true);
+        Canvas canvas = new Canvas(bitmap);
+
+        canvas.drawText(text, paddingLeft, paddingTop, paint);
+        return bitmap;
+    }
+
+}
