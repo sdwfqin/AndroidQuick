00deda98a7ae72f4a419fa94ccd4d8fd531de88e
zhangqin <sdwfqin@gmail.com>
2020-04-10 22:46:56 +0800

    Update QMUI

--- app/src/main/java/com/sdwfqin/quickseed/base/SampleBaseActivity.java
+++ app/src/main/java/com/sdwfqin/quickseed/base/SampleBaseActivity.java
@@ -22,6 +22,8 @@ public abstract class SampleBaseActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setSkinManager(QMUISkinManager.defaultInstance(this));
     }
 
     @Override
@@ -32,16 +34,20 @@ public abstract class SampleBaseActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        QMUISkinManager.defaultInstance(this).addSkinChangeListener(mOnSkinChangeListener);
+        if (getSkinManager() != null) {
+            getSkinManager().addSkinChangeListener(mOnSkinChangeListener);
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        QMUISkinManager.defaultInstance(this).removeSkinChangeListener(mOnSkinChangeListener);
+        if (getSkinManager() != null) {
+            getSkinManager().removeSkinChangeListener(mOnSkinChangeListener);
+        }
     }
 
-    private QMUISkinManager.OnSkinChangeListener mOnSkinChangeListener = (oldSkin, newSkin) -> {
+    private QMUISkinManager.OnSkinChangeListener mOnSkinChangeListener = (skinManager, oldSkin, newSkin) -> {
         if (newSkin == QMUISkinCustManager.SKIN_BLUE) {
             QMUIStatusBarHelper.setStatusBarLightMode(mContext);
         } else {
